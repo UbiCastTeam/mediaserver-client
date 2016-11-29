d1ac47b9fc27b0605444e48d96887788eee44dfe
Stéphane Diemer <stephane.diemer@ubicast.eu>
2016-11-29 11:49:26 +0100

    Fixed add media API call.

--- mediaserver_api_client.py
+++ mediaserver_api_client.py
@@ -2,13 +2,16 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016, Florent Thiery
 
+import hashlib
+import json
+import logging
+import math
 import os
+import requests
 import sys
 import time
-import math
-import hashlib
-import requests
-import json
+
+logger = logging.getLogger('mediaserver_client')
 
 MiB = 1024 * 1024
 session = None
@@ -24,15 +27,20 @@ CONFIG_DEFAULT = {
 }
 
 
-def save_config(config_dict, config_fpath):
+def save_config(config, config_fpath):
     with open(config_fpath, 'w') as config_file:
-        json.dump(config_dict, config_file, sort_keys=True, indent=4, separators=(',', ': '))
+        json.dump(config, config_file, sort_keys=True, indent=4, separators=(',', ': '))
 
 
 def read_config(config_fpath):
-    print('Reading %s' % config_fpath)
-    with open(config_fpath, 'r') as config_file:
-        return json.load(config_file)
+    logger.debug('Reading %s', config_fpath)
+    config = CONFIG_DEFAULT.copy()
+    try:
+        with open(config_fpath, 'r') as config_file:
+            config.update(json.load(config_file))
+    except Exception as e:
+        logger.error('Error while parsing configuration file, using defaults (%s).', e)
+    return config
 
 
 class MediaServerClient:
@@ -41,18 +49,19 @@ class MediaServerClient:
         if not config['VERIFY_SSL']:
             from requests.packages.urllib3.exceptions import InsecureRequestWarning
             requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-        self.UPLOAD_CHUNK_SIZE = config['UPLOAD_CHUNK_SIZE']
 
-    def request(self, url, method='get', data={}, params={}, files={}, headers={}, json=True, timeout=10):
+    def request(self, url, method='get', data=None, params=None, files=None, headers=None, json=True, timeout=10):
         global session
         if session is None:
             session = requests.Session()
 
         if method == 'get':
             req_function = session.get
+            params = params or dict()
             params['api_key'] = self.config['API_KEY']
         else:
             req_function = session.post
+            data = data or dict()
             data['api_key'] = self.config['API_KEY']
 
         req_args = {
@@ -60,83 +69,79 @@ class MediaServerClient:
             'headers': headers,
             'params': params,
             'data': data,
+            'files': files,
             'timeout': timeout,
             'proxies': self.config['PROXIES'],
             'verify': self.config['VERIFY_SSL'],
-            'files': files,
         }
-        resp = req_function(**req_args)
-        if resp.status_code != 200:
-            raise Exception('HTTP %s error on %s: %s' % (resp.status_code, url, resp.text))
-        return resp.json() if json else resp.text.strip()
+        req = req_function(**req_args)
+        if req.status_code != 200:
+            raise Exception('HTTP %s error on %s: %s' % (req.status_code, url, req.text))
+        return req.json() if json else req.text.strip()
 
     def api(self, suffix, *args, **kwargs):
-        BASE_URL = requests.compat.urljoin(self.config['SERVER_URL'], 'api/v2/')
-        suffix.lstrip('/')
-        kwargs['url'] = requests.compat.urljoin(BASE_URL, suffix)
+        kwargs['url'] = self.config['SERVER_URL'].strip('/') + '/api/v2/' + suffix.lstrip('/')
         return self.request(*args, **kwargs)
 
-    def read_in_chunks(self, file_object):
-        while True:
-            data = file_object.read(self.UPLOAD_CHUNK_SIZE)
-            if not data:
-                break
-            yield data
-
-    def chunked_upload(self, file_path, title="", category="Unsorted", **kwargs):
+    def chunked_upload(self, file_path):
+        chunk_size = self.config['UPLOAD_CHUNK_SIZE']
         total_size = os.path.getsize(file_path)
-        chunks_count = math.ceil(total_size / self.UPLOAD_CHUNK_SIZE)
+        chunks_count = math.ceil(total_size / chunk_size)
+        chunk_index = 0
         start_offset = 0
-        end_offset = min(self.UPLOAD_CHUNK_SIZE, total_size) - 1
-        upload_data = {}
+        end_offset = min(chunk_size, total_size) - 1
+        data = dict()
         md5sum = hashlib.md5()
         begin = time.time()
         with open(file_path, 'rb') as file_object:
-            for index, chunk in enumerate(self.read_in_chunks(file_object)):
-                print('Uploading chunk [%s/%s]' % (index + 1, chunks_count))
+            while True:
+                chunk = file_object.read(chunk_size)
+                if not chunk:
+                    break
+                chunk_index += 1
+                logger.debug('Uploading chunk [%s/%s]', chunk_index, chunks_count)
                 md5sum.update(chunk)
                 files = {'file': (os.path.basename(file_path), chunk)}
-                headers = {'Content-Range': 'bytes %(start_offset)s-%(end_offset)s/%(total_size)s' % locals()}
-                resp = self.api('medias/resource/upload/', method='post', data=upload_data, files=files, headers=headers)
-                if 'upload_id' not in upload_data:
-                    upload_data['upload_id'] = resp['upload_id']
-                start_offset += self.UPLOAD_CHUNK_SIZE
-                end_offset = min(end_offset + self.UPLOAD_CHUNK_SIZE, total_size - 1)
-        upload_data['md5'] = md5sum.hexdigest()
-        bandwidth = total_size*8/((time.time() - begin)*1000000)
-        print('Upload finished, average bandwidth: %.2f Mbits/s' % bandwidth)
-        resp = self.api('medias/resource/upload/complete/', method='post', data=upload_data, timeout=600)
-        metadata = {
-            'title': title,
-            'code': upload_data['upload_id'],
-            'origin': self.config['CLIENT_ID'],
-        }
-        arguments = {**metadata, **kwargs}
-        resp = self.api('medias/add/', method='post', data=arguments)
-        return resp
+                headers = {'Content-Range': 'bytes %s-%s/%s' % (start_offset, end_offset, total_size)}
+                response = self.api('medias/resource/upload/', method='post', data=data, files=files, headers=headers)
+                if 'upload_id' not in data:
+                    data['upload_id'] = response['upload_id']
+                start_offset += chunk_size
+                end_offset = min(end_offset + chunk_size, total_size - 1)
+        bandwidth = total_size * 8 / ((time.time() - begin) * 1000000)
+        logger.debug('Upload finished, average bandwidth: %.2f Mbits/s', bandwidth)
+        data['md5'] = md5sum.hexdigest()
+        response = self.api('medias/resource/upload/complete/', method='post', data=data, timeout=600)
+        return data['upload_id']
+
+    def add_media(self, title, file_path=None, **kwargs):
+        metadata = kwargs
+        metadata['title'] = title
+        metadata['origin'] = self.config['CLIENT_ID']
+        if file_path:
+            metadata['code'] = self.chunked_upload(file_path)
+        response = self.api('medias/add/', method='post', data=metadata)
+        return response
 
 
 if __name__ == '__main__':
+    log_format = '%(asctime)s %(name)s %(levelname)s %(message)s'
+    logging.basicConfig(level=logging.DEBUG, format=log_format)
+    urllib3_logger = logging.getLogger('requests.packages.urllib3')
+    urllib3_logger.setLevel(logging.WARNING)
+
     try:
         config_fpath = sys.argv[1]
     except IndexError:
         config_fpath = 'config.json'
-    try:
-        config = read_config(config_fpath)
-        for k in CONFIG_DEFAULT.keys():
-            changed = False
-            if config.get(k) is None:
-                config[k] = CONFIG_DEFAULT[k]
-                changed = True
-        if changed:
-            save_config(config, config_fpath)
-            print('Config updated and saved to %s' % config_fpath)
-    except Exception as e:
-        print('Error while parsing configuration file, using defaults (%s)' % e)
-        config = CONFIG_DEFAULT
+    config = read_config(config_fpath)
 
-    ms = MediaServerClient(config)
+    msc = MediaServerClient(config)
+    # ping
+    print(msc.api('/', method='get'))
 
+    # add media
+    # print(msc.add_media('Test multichunk upload', file_path='/tmp/test.mp4', layout='webinar', detect_slide=['0_0-640_480-750']))
+
+    # add user
     # print(ms.api('users/add/', method='post', data={'email': 'test@test.com'}))
-    # ms.chunked_upload('/tmp/test.mp4', title='Test multichunk upload', layout='webinar', detect_slide=['0_0-640_480-750'])
-    # fname = sys.argv[1]
