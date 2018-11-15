4b75c375b13d5f83024ae390439a73eeccbfbe3a
Stéphane Diemer <stephane.diemer@ubicast.eu>
2018-11-15 15:43:38 +0100

    Added examples dir.

---
+++ examples/download_all_original_files.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Script to download all best quality video files from a MediaServer.
+
+To use this script clone MediaServer client and put this file in a sub dir in the client dir.
+git clone https://github.com/UbiCastTeam/mediaserver-client
+mkdir examples
+mv "this file" mediaserver-client/examples
+'''
+import os
+import re
+import subprocess
+import sys
+
+
+if __name__ == '__main__':
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+    from mediaserver_api_client import MediaServerClient
+
+    msc = MediaServerClient(config_dict=dict(
+        SERVER_URL='https://mediaserver',
+        API_KEY='',
+        VERIFY_SSL=False,
+    ))
+    msc.check_server()
+
+    dir_path = 'videos'
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    more = True
+    start = ''
+    index = 0
+    failed = list()
+    while more:
+        print('//// Making request on latest (start=%s)' % start)
+        response = msc.api('latest/', params=dict(start=start, content='v', count=20))
+        for item in response['items']:
+            index += 1
+            print('// Media %s' % index)
+            resources = msc.api('medias/resources-list/', params=dict(oid=item['oid']))['resources']
+            resources.sort(key=lambda a: -a['file_size'])
+            best_quality = None
+            for r in resources:
+                if r['protocol'] == 'http' and r['format'] not in ('m3u8', 'youtube', 'embed'):
+                    best_quality = r
+                    break
+            video_page = msc.config['SERVER_URL'] + '/permalink/' + item['oid'] + '/'
+            if not best_quality:
+                print('WARNING: No resource file found for video "%s"!' % video_page)
+            else:
+                print('Best quality file for video "%s": %s' % (video_page, best_quality['file']))
+                name = re.sub(r'[^A-Za-z0-9]+', '-', item['title'])[:30]
+                destination = '%s/%s_%s_%sx%s.%s' % (dir_path, item['oid'], name, best_quality['width'], best_quality['height'], best_quality['format'])
+                p = subprocess.run(['wget', '--no-check-certificate', best_quality['file'], '-O', destination])
+                if p.returncode != 0:
+                    failed.append((item['oid'], best_quality['file'], destination))
+        start = response['max_date']
+        more = response['more']
+
+    if failed:
+        print('Some file download have failed:')
+        for f in failed:
+            print('%s\t%s\t' % f)
+    else:
+        print('All download have been done successfully.')
