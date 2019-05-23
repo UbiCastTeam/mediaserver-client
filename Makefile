61b57e0a8e7ed7928cd6b99abe1e120afb03e8b3
Nicolas KAROLAK <nicolas@karolak.fr>
2019-05-23 15:04:26 +0200

    use simpler and standard setup.cfg

---
+++ Makefile
@@ -0,0 +1,31 @@
+PYFILES = ms_client/ examples/
+
+all:
+
+format:
+	black ${PYFILES}
+
+lint:
+	flake8 ${PYFILES}
+
+develop:
+	pip install -e .[dev]
+
+build: clean
+	python setup.py sdist bdist_wheel
+
+install: build
+	pip install -I dist/mediaserver_api_client-*.whl
+
+publish_dry: build
+	twine check dist/*.{whl,tar.gz}
+	twine upload --repository-url https://test.pypi.org/legacy/ dist/*.{whl,tar.gz}
+
+publish: build
+	twine check dist/*.{whl,tar.gz}
+	twine upload dist/*.{whl,tar.gz}
+
+clean:
+	rm -rf .eggs/ build/ dist/ *.egg-info/
+	find . -type f -name *.pyc -delete
+	find . -type d -name __pycache__ -delete
--- setup.py
+++ setup.py
@@ -1,28 +1,6 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-import os
-from setuptools import setup
+#!/usr/bin/env python3
+
+import setuptools
 
 
-# get version without importing module
-version = None
-with open(os.path.join(os.path.dirname(__file__), 'ms_client', '__init__.py'), 'r') as fo:
-    for line in fo:
-        if line.startswith('__version__'):
-            version = line[len('__version__'):].strip(' =\'"\n\t')
-            break
-
-
-setup(
-    name='ms_client',
-    version=version,
-    description='A Python3 client to interact with an UbiCast MediaServer site.',
-    author='UbiCast',
-    url='https://github.com/UbiCastTeam/mediaserver-client',
-    license='LGPL v3',
-    packages=['ms_client'],
-    package_data={'ms_client': ['conf.json']},
-    scripts=[],
-    setup_requires=['setuptools'],
-    install_requires=['requests'],
-)
+setuptools.setup()
