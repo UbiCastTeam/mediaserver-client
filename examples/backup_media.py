dd503282521f1743054fcdab7272e4d465822bdf
Stéphane Diemer <stephane.diemer@ubicast.eu>
2019-12-19 17:54:01 +0100

    Changed backup script to use a single zip file for each media | refs #31065

--- examples/backup_media.py
+++ examples/backup_media.py
@@ -4,9 +4,12 @@
 Script to backup media (metadata and best resource) from a MediaServer.
 
 To use this script clone MediaServer client, configure it and run this file.
+By default, files are placed in a directory named "backups" in the current directory.
+
 git clone https://github.com/UbiCastTeam/mediaserver-client
 cd mediaserver-client
 python3 examples/backup_media.py --conf conf.json --date 2020-01-01
+
 
 Dependencies:
 python3-unidecode
@@ -19,6 +22,7 @@ import requests
 import subprocess
 import sys
 import unidecode
+import zipfile
 
 
 # Terminal colors
@@ -42,7 +46,7 @@ def get_repr(item):
 
 
 def get_prefix(item):
-    return unidecode.unidecode(item['title'][:57].strip()) + ' - ' + item['oid']
+    return unidecode.unidecode(item['title'][:57].strip()).replace('/', '|') + ' - ' + item['oid']
 
 
 def make_backup(msc, dir_path, limit_date, use_add_date=False, enable_delete=False):
@@ -66,52 +70,23 @@ def make_backup(msc, dir_path, limit_date, use_add_date=False, enable_delete=Fal
             media_date = datetime.datetime.strptime(item[date_field][0:10], '%Y-%m-%d').date()
             if media_date > limit_date:
                 print('No backup for media %s because creation date %s is newer than backup date %s' % (get_repr(item), item['creation'], limit_date))
-                continue
-
-            channels = msc.api('channels/path/', params=dict(oid=item['oid']))['path']
-
-            file_prefix = get_prefix(item)
-            media_backup_dir = dir_path
-            for channel in channels:
-                media_backup_dir += '/' + get_prefix(channel)
-            media_backup_dir += '/' + file_prefix
-            if not os.path.exists(media_backup_dir):
-                os.makedirs(media_backup_dir)
-
-            try:
-                download_media_best_resource(msc, item, media_backup_dir, file_prefix)
-                resource_dl_error = None
-            except Exception as e:
-                error = 'Failed to download resource: ' + str(e)
-                print('%s%s%s' % (RED, error, DEFAULT))
-                resource_dl_error = error
-
-            try:
-                download_media_metadata(msc, item, media_backup_dir, file_prefix)
-                metadata_dl_error = None
-            except Exception as e:
-                error = 'Failed to download metadata: ' + str(e)
-                print('%s%s%s' % (RED, error, DEFAULT))
-                metadata_dl_error = error
-
-            # Delete media if enable delete and resource and metadata are OK
-            if resource_dl_error or metadata_dl_error:
-                error = ''
-                if resource_dl_error:
-                    error += ' ' + resource_dl_error
-                if metadata_dl_error:
-                    error += ' ' + metadata_dl_error
-                failed.append((item, error))
-                if enable_delete:
-                    print('Media %s will not be deleted because it has not been successfully downloaded.' % get_repr(item))
             else:
-                backuped.append(item)
-                if enable_delete:
-                    try:
-                        msc.api('medias/delete/', method='post', data=dict(oid=item['oid'], delete_metadata='yes', delete_resources='yes'))
-                    except Exception:
-                        print('Failed to delete media %s')
-                print('Media %s has been deleted successfully from MediaServer.' % get_repr(item))
+                try:
+                    backup_media(item, dir_path)
+                except Exception as e:
+                    print('%s%s%s' % (RED, e, DEFAULT))
+                    failed.append((item, str(e)))
+                    if enable_delete:
+                        print('Media %s will not be deleted because it has not been successfully downloaded.' % get_repr(item))
+                else:
+                    backuped.append(item)
+                    if enable_delete:
+                        try:
+                            msc.api('medias/delete/', method='post', data=dict(oid=item['oid'], delete_metadata='yes', delete_resources='yes'))
+                        except Exception as e:
+                            print('Failed to delete media %s: %s' % (get_repr(item), e))
+                        else:
+                            print('Media %s has been deleted successfully from MediaServer.' % get_repr(item))
         start = response['max_date']
         more = response['more']
     print('Done.\n')
@@ -123,7 +98,7 @@ def make_backup(msc, dir_path, limit_date, use_add_date=False, enable_delete=Fal
     if failed:
         print('%sMedia backups failed (%s):%s' % (RED, len(failed), DEFAULT))
         for item, error in failed:
-            print('  [%sKO%s] %s:%s' % (RED, DEFAULT, get_repr(item), error))
+            print('  [%sKO%s] %s: %s' % (RED, DEFAULT, get_repr(item), error))
         print('%sSome media were not backuped.%s' % (YELLOW, DEFAULT))
         return 1
     if backuped:
@@ -133,7 +108,85 @@ def make_backup(msc, dir_path, limit_date, use_add_date=False, enable_delete=Fal
     return 0
 
 
-def download_media_best_resource(msc, item, media_backup_dir, file_prefix):
+def backup_media(item, dir_path):
+    channels = msc.api('channels/path/', params=dict(oid=item['oid']))['path']
+
+    media_chan_path = [channel['title'] for channel in channels]
+    media_backup_dir = os.path.join(dir_path, *[get_prefix(channel) for channel in channels])
+    if not os.path.exists(media_backup_dir):
+        os.makedirs(media_backup_dir)
+
+    file_prefix = get_prefix(item)
+    zip_path = os.path.join(media_backup_dir, file_prefix + '.zip')
+    metadata_zip_size = 0
+    best_resource_size = 0
+    if os.path.exists(zip_path):
+        # get file size from existing zip to skip downloads if useless
+        try:
+            zip_file = zipfile.ZipFile(zip_path, 'r')
+        except Exception as e:
+            print('%sFailed to open existing zip file: %s%s' % (RED, e, DEFAULT))
+        else:
+            for name in zip_file.namelist():
+                if name == 'metadata-size.txt':
+                    try:
+                        metadata_zip_size = int(zip_file.open(name).read())
+                    except Exception as e:
+                        print('%sFailed to parse metadata from existing zip file: %s%s' % (RED, e, DEFAULT))
+                elif name.startswith('resource -'):
+                    file_info = zip_file.getinfo(name)
+                    best_resource_size = file_info.file_size
+            zip_file.close()
+
+    try:
+        res_path = download_media_best_resource(msc, item, media_backup_dir, file_prefix, best_resource_size)
+    except Exception as e:
+        raise Exception('Failed to download resource: %s' % e)
+
+    try:
+        if res_path:
+            metadata_zip_size = 0  # force zip download if the best resource has changed
+        meta_path = download_media_metadata(msc, item, media_backup_dir, file_prefix, metadata_zip_size)
+    except Exception as e:
+        raise Exception('Failed to download metadata: %s' % e)
+
+    if res_path or meta_path:
+        # the metadata or the best resource has changed, put resource in zip and update info
+        if not meta_path and res_path:
+            raise Exception('The metadata should have been downloaded.')
+        metadata_size = os.path.getsize(meta_path)
+        try:
+            zip_file = zipfile.ZipFile(meta_path, 'r')
+        except Exception as e:
+            raise Exception('Failed to open downloaded zip file: %s' % e)
+
+        # add resource in zip and some other informations
+        try:
+            zip_file = zipfile.ZipFile(meta_path, 'a')
+        except Exception as e:
+            raise Exception('Failed to open downloaded zip file: %s' % e)
+        zip_file.writestr('metadata-size.txt', str(metadata_size))
+        zip_file.writestr('metadata-path.txt', '/'.join(media_chan_path))
+        if res_path:
+            zip_file.write(res_path, os.path.basename(res_path))
+        zip_file.close()
+
+        # CRC check of zip file
+        zip_file = zipfile.ZipFile(meta_path, 'r')
+        files_with_error = zip_file.testzip()
+        if files_with_error:
+            raise Exception('Some files have errors in the zip file: %s' % files_with_error)
+        zip_file.close()
+
+        # rename zip file and remove temporary files
+        os.rename(meta_path, zip_path)
+        if res_path:
+            os.remove(res_path)
+    else:
+        print('Media was already correctly backuped.')
+
+
+def download_media_best_resource(msc, item, media_backup_dir, file_prefix, local_size=0):
     if item['oid'][0] != 'v':
         return  # item is not a video
     resources = msc.api('medias/resources-list/', params=dict(oid=item['oid']))['resources']
@@ -152,18 +205,20 @@ def download_media_best_resource(msc, item, media_backup_dir, file_prefix):
         raise Exception('Could not download any resource from list: %s.' % resources)
 
     print('Best quality file for video %s: %s' % (get_repr(item), best_quality['file']))
-    destination_resource = '%s/%s - %sx%s.%s' % (media_backup_dir, file_prefix, best_quality['width'], best_quality['height'], best_quality['format'])
+    destination_resource = os.path.join(media_backup_dir, 'resource - %s - %sx%s.%s' % (file_prefix, best_quality['width'], best_quality['height'], best_quality['format']))
 
     if best_quality['format'] in ('youtube', 'embed'):
         # dump youtube video id or embed code to a file
         with open(destination_resource, 'w') as fo:
             fo.write(best_quality['file'])
     else:
+        # download resource
         url_resource = msc.api('download/', params=dict(oid=item['oid'], url=best_quality['file'], redirect='no'))['url']
         if os.path.exists(destination_resource):
             local_size = os.path.getsize(destination_resource)
+        if local_size:
             req = requests.head(url_resource, verify=False)
-            if local_size == int(req.headers.get('Content-Length', '0')):
+            if req.headers.get('Content-Length') == str(local_size):
                 print('File is already downloaded: "%s".' % destination_resource)
                 return
 
@@ -171,12 +226,21 @@ def download_media_best_resource(msc, item, media_backup_dir, file_prefix):
         p_resource = subprocess.run(['wget', '--no-check-certificate', url_resource, '-O', destination_resource])
         if p_resource.returncode != 0:
             raise Exception('The wget command exited with code %s.' % p_resource.returncode)
+    return destination_resource
 
 
-def download_media_metadata(msc, item, media_backup_dir, file_prefix):
-    destination_metadata = '%s/%s.zip' % (media_backup_dir, file_prefix)
+def download_media_metadata(msc, item, media_backup_dir, file_prefix, local_size=0):
+    if local_size:
+        params = dict(oid=item['oid'], annotations='all', resources='no')
+        req = msc.api('medias/get/zip/', method='head', params=params, timeout=3600)
+        if req.headers.get('Content-Length') == str(local_size):
+            print('Skipping download of zip file for %s because the file already exists and has the correct size.' % item['oid'])
+            return
+
+    destination_metadata = os.path.join(media_backup_dir, 'metadata %s.zip' % file_prefix)
     path = msc.download_metadata_zip(item['oid'], destination_metadata, include_annotations='all', include_resources_links='no')
     print('Metadata downloaded for media %s: "%s".' % (get_repr(item), path))
+    return path
 
 
 if __name__ == '__main__':
