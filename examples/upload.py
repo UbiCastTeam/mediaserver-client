794a772b43d353ef62e4c7abd2bc362a851087ce
Florent Thiéry <florent.thiery@ubicast.eu>
2022-02-24 16:03:17 +0100

    add support for uploading from HTTP URLs in example upload script

--- examples/upload.py
+++ examples/upload.py
@@ -3,9 +3,10 @@
 '''
 Script to upload videos
 
-./examples/upload.py --config beta.json --input test.mp4 --title "mytitle" --channel "mscpath-A/B/C" --speaker-email "test@test.com"
+./examples/upload.py --config mynudgis.json --input test.mp4 --title "mytitle" --channel "mscpath-A/B/C" --speaker-email "test@test.com"
 '''
 import argparse
+from urllib.parse import urlparse
 import logging
 import os
 import sys
@@ -20,7 +21,7 @@ if __name__ == '__main__':
         '--input',
         type=str,
         required=True,
-        help='Path to file to upload'
+        help='Path to file to upload, or HTTP URL of media file to import'
     )
 
     parser.add_argument(
@@ -62,14 +63,35 @@ if __name__ == '__main__':
 
     msc = MediaServerClient(args.config)
     msc.check_server()
-    resp = msc.add_media(
-        file_path=args.input,
-        title=args.title,
-        channel=args.channel,
-        speaker_email=args.speaker_email,
-        progress_callback=print_progress,
-    )
+
+    params = {
+        'title': args.title,
+        'channel': args.channel,
+        'speaker_email': args.speaker_email,
+        'progress_callback': print_progress,
+    }
+
+    # depending on the case (local or remote file) the API argument is not the same
+    input_arg = None
+    if os.path.isfile(args.input):
+        input_arg = 'file_path'
+    else:
+        u = urlparse(args.input)
+        if u.scheme and 'http' in u.scheme:
+            input_arg = 'file_url'
+            # we do not need to specify a title in case of file upload because the
+            # client will use the filename as fallback, but it is not supported for URLs
+            params['title'] = u.path.split('/')[-1]
+
+    if input_arg is None:
+        print(f'Unkown or unsupported input {args.input}')
+        sys.exit(1)
+
+    params[input_arg] = args.input
+
+    resp = msc.add_media(**params)
+
     if resp['success']:
-        print(f'File {args.input} upload finished, object id is {resp["oid"]}')
+        print(f'File {args.input} upload finished, object id is {resp["oid"]} and can be accessed at {msc.conf["SERVER_URL"]}/permalink/{resp["oid"]}')
     else:
         print(f'Upload of {args.input} failed: {resp}')
