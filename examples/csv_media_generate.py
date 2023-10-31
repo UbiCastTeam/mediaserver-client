b520b7325fc5c999e7693e2f0c88dcacb7aa38d6
Florent Thiéry <florent.thiery@ubicast.eu>
2023-10-31 14:49:12 +0100

    update csv_media_* scripts to use /catalog/get-all API call

--- examples/csv_media_generate.py
+++ examples/csv_media_generate.py
@@ -1,93 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-'''
+"""
 Script to generate a CSV file for metadata from all media in the database
-'''
-import csv
+"""
 import os
 import sys
 
 
-def parse_duration(dur):
-    #'1 h 51 m 21 s'
-    fields = dur.split(' ')
-    hours_index = minutes_index = seconds_index = None
-    if len(fields) == 6:
-        hours_index = 0
-        minutes_index = 2
-        seconds_index = 4
-    elif len(fields) == 4:
-        minutes_index = 0
-        seconds_index = 2
-    elif len(fields) == 2:
-        seconds_index = 0
-
-    hours = int(fields[hours_index]) if hours_index is not None else 0
-    minutes = int(fields[minutes_index]) if minutes_index is not None else 0
-    seconds = int(fields[seconds_index]) if seconds_index is not None else 0
-
-    total_seconds = hours * 3600 + minutes * 60 + seconds
-    return total_seconds
-
-
 def generate_csv(msc, csv_path):
-    fields = [
-        'oid',
-        'title',
-        'duration',
-        'storage_used',
-        'tree',
-        'categories',
-        'language',
-        'speaker_email',
-        'creation',
-        'location',
-        'validated',
-        'type',
-        'layout',
-        'origin',
-        'comments',
-        'views',
-        'views_last_month',
-    ]
-
-    with open(csv_path, 'w') as f:
-        more = True
-        start = ''
-        index = 0
-
-        writer = csv.DictWriter(f, fieldnames=fields, delimiter='\t')
-        writer.writeheader()
-
-        while more:
-            print(f'//// Making request on latest (start={start})')
-            response = msc.api('latest/', params=dict(start=start, content='v', count=100))
-            for item in response['items']:
-                index += 1
-                print(f'// Media {index}: {item["oid"]}')
-
-                params = {
-                    'oid': item['oid'],
-                    'path': 'yes',
-                    'full': 'yes',
-                }
-                data = msc.api('medias/get/', params=params)['info']
-                row = {}
-                # only copy fields we want
-                for field in fields:
-                    if data.get(field):
-                        if field == 'duration':
-                            row[field] = parse_duration(data[field])
-                        else:
-                            row[field] = data[field]
-                path = '/'.join([item['title'] for item in data['path']])
-                row['tree'] = path
-                writer.writerow(row)
-            start = response['max_date']
-            more = response['more']
+    with open(csv_path, "w") as f:
+        print("Fetching catalog")
+        catalog_csv = msc.api(
+            "catalog/get-all/", params={"format": "csv"}, parse_json=False
+        )
+        print(f"Writing {csv_path}")
+        f.write(catalog_csv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     from ms_client.client import MediaServerClient
 
@@ -97,8 +27,8 @@ if __name__ == '__main__':
 
     csv_path = f'media-{msc.conf["SERVER_URL"].split("://")[1]}.csv'
     if os.path.isfile(csv_path):
-        print(f'File {csv_path} already exists, exiting with error')
+        print(f"File {csv_path} already exists, exiting with error")
         sys.exit(1)
 
     generate_csv(msc, csv_path)
-    print(f'Finished writing {csv_path}')
+    print(f"Finished writing {csv_path}")
--- examples/csv_media_stats.py
+++ examples/csv_media_stats.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 import argparse
-import logging
 import csv
+import logging
+import sys
 
 from datetime import datetime
 
+csv.field_size_limit(sys.maxsize)  # support huge fields
+
 
 def setup_logging(verbose=False):
-    logging.addLevelName(logging.ERROR, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
-    logging.addLevelName(logging.WARNING, "\033[1;33m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
-    level = getattr(logging, 'DEBUG' if verbose else 'INFO')
+    logging.addLevelName(
+        logging.ERROR, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.ERROR)
+    )
+    logging.addLevelName(
+        logging.WARNING, "\033[1;33m%s\033[1;0m" % logging.getLevelName(logging.WARNING)
+    )
+    level = getattr(logging, "DEBUG" if verbose else "INFO")
     logging.basicConfig(
         level=level,
         format="%(asctime)s %(levelname)-8s %(message)s",
@@ -22,12 +29,12 @@ def get_percent(index, total):
 
 
 def get_percent_string(val, total, do_format=None):
-    if do_format == 'size':
-        return f'{format_bytes(val)} / {format_bytes(total)} ({get_percent(val, total):.1f}%)'
-    elif do_format == 'time':
-        return f'{format_seconds(val)} / {format_seconds(total)} ({get_percent(val, total):.1f}%)'
+    if do_format == "size":
+        return f"{format_bytes(val)} / {format_bytes(total)} ({get_percent(val, total):.1f}%)"
+    elif do_format == "time":
+        return f"{format_seconds(val)} / {format_seconds(total)} ({get_percent(val, total):.1f}%)"
     else:
-        return f'{val} / {total} ({get_percent(val, total):.1f}%)'
+        return f"{val} / {total} ({get_percent(val, total):.1f}%)"
 
 
 def format_seconds(seconds):
@@ -41,16 +48,21 @@ def format_bytes(size):
     # 2**10 = 1024
     power = 2**10
     n = 0
-    power_labels = {0: '', 1: 'kilo', 2: 'mega', 3: 'giga', 4: 'tera'}
+    power_labels = {0: "", 1: "kilo", 2: "mega", 3: "giga", 4: "tera"}
     while size > power:
         size /= power
         n += 1
-    return f'{round(size, 1)} {power_labels[n]}bytes'
+    return f"{round(size, 1)} {power_labels[n]}bytes"
 
 
-def print_dict_stats(d, title, threshold_percent=0, do_format=None,):
+def print_dict_stats(
+    d,
+    title,
+    threshold_percent=0,
+    do_format=None,
+):
     d = dict(sorted(d.items(), key=lambda item: item[1], reverse=True))
-    print(f'\n**** {title}:')
+    print(f"\n**** {title}:")
     total = sum(d.values())
     skipped = 0
     for key, val in d.items():
@@ -59,12 +71,14 @@ def print_dict_stats(d, title, threshold_percent=0, do_format=None,):
             if threshold_percent and percent <= threshold_percent:
                 skipped += val
             else:
-                print(f'{key}: {get_percent_string(val, total, do_format)}')
+                print(f"{key}: {get_percent_string(val, total, do_format)}")
     if skipped:
-        print(f'Various (below {threshold_percent}%: {get_percent_string(skipped, total)}')
+        print(
+            f"Various (below {threshold_percent}%: {get_percent_string(skipped, total)}"
+        )
 
 
-logger = logging.getLogger('csv-media-stats')
+logger = logging.getLogger("csv-media-stats")
 
 
 class Stats:
@@ -76,52 +90,57 @@ class Stats:
         self.display_stats()
 
     def parse_options(self):
-        for key in ['start_date', 'end_date']:
+        for key in ["start_date", "end_date"]:
             date = self.options.get(key)
             if date:
-                self.options[key] = datetime.strptime(date, '%Y-%m-%d')
+                self.options[key] = datetime.strptime(date, "%Y-%m-%d")
 
     def read_csv(self):
         self.media_list = list()
-        with open(self.options['input']) as f:
-            reader = csv.DictReader(f, delimiter='\t')
+        with open(self.options["input"]) as f:
+            reader = csv.DictReader(f, delimiter=",")
             for row in reader:
-                try:
-                    media = self.filter_by_date(row)
-                    if media:
-                        self.media_list.append(media)
-                except Exception as e:
-                    logger.warning(f'Failed to parse row {row}: {e}')
-        logging.info(f'Found {len(self.media_list)} media, skipped {self.skipped}')
+                if row["type"] == "video":
+                    try:
+                        media = self.filter_by_date(row)
+                        if media:
+                            self.media_list.append(media)
+                    except Exception as e:
+                        logger.warning(f"Failed to parse row {row}: {e}")
+        logging.info(f"Found {len(self.media_list)} media, skipped {self.skipped}")
 
     def filter_by_date(self, row):
-        creation = row['creation'] = datetime.strptime(row['creation'], '%Y-%m-%d %H:%M:%S')
-        if self.options['start_date'] and creation < self.options['start_date']:
-            logging.debug(f'Skipping {row} because it was created before the start date')
+        creation = row["creation"] = datetime.strptime(
+            row["creation"], "%Y-%m-%d %H:%M:%S"
+        )
+        if self.options["start_date"] and creation < self.options["start_date"]:
+            logging.debug(
+                f"Skipping {row} because it was created before the start date"
+            )
             self.skipped += 1
             return
-        if self.options['end_date'] and creation > self.options['end_date']:
+        if self.options["end_date"] and creation > self.options["end_date"]:
             self.skipped += 1
-            logging.debug(f'Skipping {row} because it was created after the end date')
+            logging.debug(f"Skipping {row} because it was created after the end date")
             return
 
         return row
 
     def display_stats(self):
         upload_types_count = {
-            'hardware': 0,
-            'webstudio': 0,
-            'obs': 0,
-            'upload': 0,
-            'embed': 0,
-            'external-resource': 0,
-            'youtube': 0,
-            'videoconferencing': 0,
-            'mediaimport': 0,
-            'mediasite-migration': 0,
-            'mediaserver-client': 0,
-            'default': 0,
-            'empty': 0,
+            "hardware": 0,
+            "webstudio": 0,
+            "obs": 0,
+            "upload": 0,
+            "embed": 0,
+            "external-resource": 0,
+            "youtube": 0,
+            "videoconferencing": 0,
+            "mediaimport": 0,
+            "mediasite-migration": 0,
+            "mediaserver-client": 0,
+            "unspecified": 0,
+            "empty": 0,
         }
 
         upload_types_duration = dict(upload_types_count)
@@ -129,8 +148,8 @@ class Stats:
         upload_types_size = dict(upload_types_count)
 
         media_type = {
-            'original': 0,
-            'trimming': 0,
+            "original": 0,
+            "trimming": 0,
         }
 
         hardware_count = {}
@@ -140,55 +159,62 @@ class Stats:
         speakers_duration = {}
 
         for media in self.media_list:
-            origin = media['origin']
-            speaker_email = media['speaker_email']
-            duration_seconds = int(media['duration']) if media['duration'] else 0
-            size_bytes = int(media['storage_used'])
+            origin = media["origin"]
+            speaker_email = media["speaker_email"]
+            duration_seconds = int(media["duration_s"]) if media["duration_s"] else 0
+            size_bytes = int(media["storage_used"])
 
             try:
-                if 'trimming-' in origin:
-                    media_type['trimming'] += 1
-                    origin = origin.split(' trimming')[0]
+                if "trimming-" in origin:
+                    media_type["trimming"] += 1
+                    origin = origin.split(" trimming")[0]
                 else:
-                    media_type['original'] += 1
+                    media_type["original"] += 1
 
-                if origin.startswith('miris-box-') or origin.startswith('easycast-'):
-                    mtype = 'hardware'
-                    serial, version = origin.split('_')
+                if origin.startswith("miris-box-") or origin.startswith("easycast-"):
+                    mtype = "hardware"
+                    serial, version = origin.split("_")
 
                     hardware_count.setdefault(serial, 0)
                     hardware_count[serial] += 1
 
                     hardware_duration.setdefault(serial, 0)
                     hardware_duration[serial] += duration_seconds
-                elif origin in ['Manual (form: AddMediaWithFileForm)', 'Manual (form: AddVODByUploadForm)']:
-                    mtype = 'upload'
-                elif origin == 'Manual (form: AddVODWithEmbedForm)':
-                    mtype = 'embed'
-                elif origin == 'Manual (form: AddVODWithResourcesForm)':
-                    mtype = 'external-resource'
-                elif origin == 'Manual (form: AddVODWithYouTubeForm)':
-                    mtype = 'youtube'
-                elif origin == 'Manual (form: AddVODEmptyForm)':
-                    mtype = 'empty'
-                elif origin.startswith('webstudio_'):
-                    #webstudio_linux_chromium_102
-                    mtype = 'webstudio'
-                    #ws, os, browser, browser_ver = origin.split('_')
-                elif origin.startswith('zoom-') \
-                        or origin.startswith('msteams-') \
-                        or origin in ['teams', 'microsoft_teams']:
-                    mtype = 'videoconferencing'
-                elif origin == 'nudgis-obs-plugin':
-                    mtype = 'obs'
-                elif origin.startswith('mediaimport-'):
-                    mtype = 'mediaimport'
-                elif origin in ['mediatransfer', 'mediasite-migration-client']:
-                    mtype = 'mediasite-migration'
-                elif origin == 'python-api-client':
-                    mtype = 'mediaserver-client'
-                elif origin == 'API':
-                    mtype = 'default'
+                elif origin in [
+                    "Manual (form: AddMediaWithFileForm)",
+                    "Manual (form: AddVODByUploadForm)",
+                    "Manual (form: AddVODWithMetadataZipForm)",
+                ]:
+                    mtype = "upload"
+                elif origin == "Manual (form: AddVODWithEmbedForm)":
+                    mtype = "embed"
+                elif origin == "Manual (form: AddVODWithResourcesForm)":
+                    mtype = "external-resource"
+                elif origin == "Manual (form: AddVODWithYouTubeForm)":
+                    mtype = "youtube"
+                elif origin == "Manual (form: AddVODEmptyForm)":
+                    mtype = "empty"
+                elif origin.startswith("webstudio_"):
+                    # webstudio_linux_chromium_102
+                    mtype = "webstudio"
+                    # ws, os, browser, browser_ver = origin.split('_')
+                elif (
+                    origin.startswith("zoom")
+                    or origin.startswith("msteams-")
+                    or origin in ["teams", "microsoft_teams"]
+                    or origin == "big_blue_button"
+                ):
+                    mtype = "videoconferencing"
+                elif origin == "nudgis-obs-plugin":
+                    mtype = "obs"
+                elif origin.startswith("mediaimport-"):
+                    mtype = "mediaimport"
+                elif origin in ["mediatransfer", "mediasite-migration-client"]:
+                    mtype = "mediasite-migration"
+                elif origin == "python-api-client":
+                    mtype = "mediaserver-client"
+                elif origin in ["API", ""]:
+                    mtype = "unspecified"
                 else:
                     logging.warning(f'Unsupported origin "{origin}" for {media}')
 
@@ -197,7 +223,7 @@ class Stats:
                 upload_types_size[mtype] += size_bytes
 
             except Exception as e:
-                logging.warning(f'Failed to analyze origin for {media}: {e}')
+                logging.warning(f"Failed to analyze origin for {media}: {e}")
 
             if speaker_email:
                 speakers_count.setdefault(speaker_email, 0)
@@ -205,43 +231,37 @@ class Stats:
                 speakers_duration.setdefault(speaker_email, 0)
                 speakers_duration[speaker_email] += duration_seconds
 
-        print_dict_stats(upload_types_count, 'Count by type')
-        print_dict_stats(upload_types_duration, 'Duration by type', do_format='time')
-        print_dict_stats(upload_types_size, 'Size by type', do_format='size')
-        print_dict_stats(media_type, 'Media type')
-        print_dict_stats(hardware_count, 'Count by system')
-        print_dict_stats(hardware_duration, 'Duration by system', do_format='time')
-        print_dict_stats(speakers_count, 'Count by speaker')
-        print_dict_stats(speakers_duration, 'Duration by speaker', do_format='time')
+        print_dict_stats(upload_types_count, "Count by type")
+        print_dict_stats(upload_types_duration, "Duration by type", do_format="time")
+        print_dict_stats(upload_types_size, "Size by type", do_format="size")
+        print_dict_stats(media_type, "Media type")
+        print_dict_stats(hardware_count, "Count by system")
+        print_dict_stats(hardware_duration, "Duration by system", do_format="time")
+        print_dict_stats(speakers_count, "Count by speaker")
+        print_dict_stats(speakers_duration, "Duration by speaker", do_format="time")
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        help="set verbosity to DEBUG",
-        action="store_true"
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
     parser.add_argument(
-        '--input',
+        "-v", "--verbose", help="set verbosity to DEBUG", action="store_true"
+    )
+
+    parser.add_argument("--input", type=str, required=True, help="Path to CSV file")
+
+    parser.add_argument(
+        "--start-date",
         type=str,
-        required=True,
-        help='Path to CSV file'
+        help='Only keep media created after this date, e.g. "2022-01-31"',
     )
 
     parser.add_argument(
-        '--start-date',
+        "--end-date",
         type=str,
-        help='Only keep media created after this date, e.g. "2022-01-31"'
-    )
-
-    parser.add_argument(
-        '--end-date',
-        type=str,
-        help='Only keep media created before this date, e.g. "2022-01-31"'
+        help='Only keep media created before this date, e.g. "2022-01-31"',
     )
 
     args = parser.parse_args()
