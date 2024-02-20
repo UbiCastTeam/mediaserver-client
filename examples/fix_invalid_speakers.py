f1938f1e94141966dd5dcd1bf6f5b13b1fc05aca
Thomas <13025557+thomas-mckay@users.noreply.github.com>
2024-02-20 15:36:48 +0100

    Add fix_invalid_speakers script | refs #39299 (#28)

---
+++ examples/fix_invalid_speakers.py
@@ -0,0 +1,391 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+import argparse
+import csv
+from dataclasses import dataclass, field, fields
+from datetime import date
+from itertools import zip_longest
+import logging
+import os
+from pathlib import Path
+import sys
+from typing import NamedTuple, Optional
+
+try:
+    from ms_client.client import MediaServerClient
+except ModuleNotFoundError:
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    from ms_client.client import MediaServerClient
+
+
+logger = logging.getLogger(__name__)
+
+
+class Speaker(NamedTuple):
+    email: Optional[str]
+    id: Optional[str]
+    name: Optional[str]
+
+
+@dataclass
+class CSVSpeakerData:
+    email: str
+    ids: set[str] = field(default_factory=set)
+    names: set[str] = field(default_factory=set)
+    url: Optional[str] = None
+    reasons: set[str] = field(default_factory=set)
+    corrected_email: Optional[str] = None
+    corrected_id: Optional[str] = None
+    corrected_name: Optional[str] = None
+
+    @classmethod
+    def get_csv_header(cls) -> list[str]:
+        return [f.name for f in fields(cls)]
+
+    @classmethod
+    def from_csv_row(cls, csv_row: dict[str, Optional[str]]) -> CSVSpeakerData:
+        return cls(
+            email=csv_row['email'].strip(),
+            ids=set(
+                clean_spk_id
+                for spk_id in csv_row['ids'].split(',')
+                if (clean_spk_id := spk_id.strip())
+            ),
+            names=set(
+                clean_name
+                for name in csv_row['names'].split(',')
+                if (clean_name := name.strip())
+            ),
+            url=csv_row['url'].strip(),
+            reasons=set(
+                clean_reason
+                for reason in csv_row['reasons'].split(',')
+                if (clean_reason := reason.strip())
+            ),
+            corrected_email=csv_row['corrected_email'].strip(),
+            corrected_id=csv_row['corrected_id'].strip(),
+            corrected_name=csv_row['corrected_name'].strip(),
+        )
+
+    def to_csv_row(self) -> dict[str, Optional[str]]:
+        return {
+            'email': self.email,
+            'ids': ', '.join(sorted(self.ids)),
+            'names': ', '.join(sorted(self.names)),
+            'url': self.url,
+            'reasons': ', '.join(sorted(self.reasons)),
+            'corrected_email': self.corrected_email,
+            'corrected_id': self.corrected_id,
+            'corrected_name': self.corrected_name,
+        }
+
+
+def _get_speakers_by_oid(msc: MediaServerClient) -> dict[str, list[Speaker]]:
+    speakers = {}
+    catalog = msc.api('catalog/get-all/', params={'format': 'flat', 'timings': 'yes'})
+    for key in ['channels', 'videos', 'lives', 'photos']:
+        for obj in catalog.get(key, []):
+            oid = obj['oid']
+            if key == 'channels':
+                obj = obj.get('default_settings')
+                if not obj:
+                    continue
+            speakers_ids, speakers_emails, speakers_names = [], [], []
+            if obj.get('speaker_id'):
+                speakers_ids = [
+                    speaker_id.strip()
+                    for speaker_id in obj.get('speaker_id').split('|')
+                ]
+            if obj.get('speaker_email'):
+                speakers_emails = [
+                    speaker_email.strip()
+                    for speaker_email in obj.get('speaker_email').split('|')
+                ]
+            if obj.get('speaker'):
+                speakers_names = [
+                    speaker_name.strip()
+                    for speaker_name in obj.get('speaker').split('|')
+                ]
+            if len({
+                len(lst)
+                for lst in [speakers_emails, speakers_ids, speakers_names]
+                if lst
+            }) > 1:
+                logger.error(
+                    f'Media "{oid}" will be ignored because its speakers '
+                    f'cannot be parsed reliably: {[speakers_emails, speakers_ids, speakers_names]}'
+                )
+                continue
+            speakers[oid] = [
+                Speaker(speaker_email, speaker_id, speaker_name)
+                for speaker_email, speaker_id, speaker_name in zip_longest(
+                    speakers_emails, speakers_ids, speakers_names
+                )
+            ]
+    return speakers
+
+
+def _get_users(msc: MediaServerClient, page_size=500):
+    users = []
+    offset = 0
+    response = msc.api('users/', params={'limit': page_size, 'offset': offset})
+    while response['users']:
+        users += response['users']
+        offset += page_size
+        if len(response['users']) < page_size:
+            break
+        response = msc.api('users/', params={'limit': page_size, 'offset': offset})
+    return users
+
+
+def _list_invalid_speakers(msc: MediaServerClient, csv_file: Path):
+    speakers_by_oid = _get_speakers_by_oid(msc)
+    valid_emails = {
+        email: user
+        for user in _get_users(msc)
+        if (email := (user.get('email') or '').strip())
+    }
+
+    speaker_data_by_email: dict[str, CSVSpeakerData] = {}
+    for oid, speakers in speakers_by_oid.items():
+        for speaker in speakers:
+            if speaker.email:
+                data = speaker_data_by_email.setdefault(
+                    speaker.email,
+                    CSVSpeakerData(speaker.email)
+                )
+                if speaker.id:
+                    data.ids.add(speaker.id)
+                if speaker.name:
+                    data.names.add(speaker.name)
+                data.url = f'{msc.conf["SERVER_URL"]}/permalink/{oid}/'
+
+    with csv_file.open('w', newline='') as csvfile:
+        speakers_writer = csv.DictWriter(
+            csvfile,
+            fieldnames=CSVSpeakerData.get_csv_header(),
+            delimiter=',',
+            quotechar='"',
+            quoting=csv.QUOTE_MINIMAL
+        )
+        speakers_writer.writeheader()
+        for speaker_data in speaker_data_by_email.values():
+            if speaker_data.email in valid_emails:
+                user = valid_emails[speaker_data.email]
+                user_speaker_id = (user.get('speaker_id') or '').strip()
+                user_fullname = ((user.get('first_name') or '') + ' ' + (user.get('last_name') or '')).strip()
+                if user_speaker_id and speaker_data.ids and not any(
+                    spk_id == user_speaker_id
+                    for spk_id in speaker_data.ids
+                ):
+                    speaker_data.reasons.add('INVALID_ID')
+                if user_fullname and speaker_data.names and not any(
+                    name == user_fullname
+                    for name in speaker_data.names
+                ):
+                    speaker_data.reasons.add('INVALID_NAME')
+            else:
+                speaker_data.reasons.add('INVALID_EMAIL')
+            if len(speaker_data.ids) > 1:
+                speaker_data.reasons.add('MULTIPLE_ID')
+            if len(speaker_data.names) > 1:
+                speaker_data.reasons.add('MULTIPLE_NAME')
+            if speaker_data.reasons:
+                speakers_writer.writerow(speaker_data.to_csv_row())
+
+
+def _fix_invalid_speakers(
+    msc: MediaServerClient,
+    csv_file: Path,
+    apply: bool = False
+):
+    with csv_file.open('r', newline='') as csvfile:
+        speakers_reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+        corrections: dict[str, CSVSpeakerData] = {}
+        for row in speakers_reader:
+            correction = CSVSpeakerData.from_csv_row(row)
+            corrections[correction.email] = correction
+
+    speakers_by_oid = _get_speakers_by_oid(msc)
+    corrected_speakers_by_oid = {}
+    for oid, current_speakers in speakers_by_oid.items():
+        corrected_speakers = []
+        for speaker in current_speakers:
+            if speaker.email not in corrections:
+                logger.debug(f'"{speaker.email}" not in CSV. Skipping...')
+                corrected_speakers.append(speaker)
+                continue
+            correction = corrections[speaker.email]
+            if (
+                not correction.corrected_email
+                and not correction.corrected_id
+                and not correction.corrected_name
+            ):
+                logger.debug(f'No corrections for "{speaker.email}". Skipping...')
+                corrected_speakers.append(speaker)
+                continue
+
+            corrected_email = speaker.email
+            if correction.corrected_email == 'DELETE':
+                corrected_email = ''
+            elif correction.corrected_email:
+                corrected_email = correction.corrected_email
+            corrected_id = speaker.id
+            if correction.corrected_id == 'DELETE':
+                corrected_id = ''
+            elif correction.corrected_id:
+                corrected_id = correction.corrected_id
+            corrected_name = speaker.name
+            if correction.corrected_name == 'DELETE':
+                corrected_name = ''
+            elif correction.corrected_name:
+                corrected_name = correction.corrected_name
+            if any((corrected_email, corrected_id, corrected_name)):
+                corrected_speakers.append(
+                    Speaker(corrected_email, corrected_id, corrected_name)
+                )
+        if current_speakers != corrected_speakers:
+            # Integrity check
+            emails = [spk.email for spk in corrected_speakers if spk.email]
+            ids = [spk.id for spk in corrected_speakers if spk.id]
+            if len(emails) > len(set(emails)) or len(ids) > len(set(ids)):
+                logger.error(
+                    'Your corrections would lead to a speaker duplicate '
+                    f'on media "{oid}": {current_speakers=} / {corrected_speakers=}. '
+                    'This media will not be updated automatically by this script. '
+                    'You might need to surgically edit this media by hand or fix your CSV file. '
+                    'Alternatively, you might need to let the script run once to fix all other '
+                    'medias and treat these errors in a separate run.'
+                )
+            else:
+                corrected_speakers_by_oid[oid] = corrected_speakers
+
+    for oid, corrected_speakers in corrected_speakers_by_oid.items():
+        payload = {
+            'speaker_email': '|'.join(spk.email for spk in corrected_speakers),
+            'speaker_id': '|'.join(spk.id for spk in corrected_speakers),
+            'speaker': '|'.join(spk.name for spk in corrected_speakers),
+        }
+        if apply:
+            if oid[0] == 'c':
+                payload['channel_oid'] = oid
+                url = 'settings/defaults/metadata/edit/'
+            else:
+                payload['oid'] = oid
+                url = 'medias/edit/'
+            msc.api(url, method='post', data=payload)
+            logger.info(
+                f'Media "{oid}" has been updated: '
+                f'old_speakers={speakers_by_oid[oid]}, new_speakers={corrected_speakers}'
+            )
+        else:
+            logger.info(
+                f'[Dry-run] Media "{oid}" would have been updated: '
+                f'old_speakers={speakers_by_oid[oid]}, new_speakers={corrected_speakers}'
+            )
+
+
+def fix_invalid_speakers(sys_args):
+    parser = argparse.ArgumentParser(
+        'fix_invalid_speakers',
+        description=(
+            'This script has 2 modes: "list" mode and "fix" mode. You should run the '
+            'script in "list" mode first. This will output a CSV file of invalid speakers.\n'
+            '- the first column is the speaker email.\n'
+            '- the second column is the associated speaker_id(s).\n'
+            '- the third column is the associated speaker_name(s).\n'
+            '- the fourth column has one example object_id that this speaker is associated to.\n'
+            '- the fifth column is the reason(s) why this speaker was flagged as invalid. This '
+            'column can list up to 5 reasons:\n'
+            '\t* INVALID_EMAIL: the speaker email cannot be found in the user database. This can '
+            'be the result of a user changing their email address or a former user account being '
+            'deleted.\n'
+            '\t* INVALID_ID: the speaker email belongs to a user that has a different speaker_id.\n'
+            '\t* MULTIPLE_ID: the speaker email is associated with multiple user ids. This is '
+            'usually the result of miss-typed input data.\n'
+            '\t* INVALID_NAME: the speaker email belongs to a user that has a different name.\n'
+            '\t* MULTIPLE_NAME: the speaker email is associated with multiple user names. This is '
+            'usually the result of miss-typed input data.\n'
+            '- the sixth column is left empty. You should fill this column with the corrected '
+            'email address for this speaker. Leave this column empty if the email address does '
+            'not need to be modified. Use the special token "DELETE" to blank the email address '
+            'for this speaker (warning: this will affect as many medias as this speaker email is '
+            'associated with).\n'
+            '- the seventh column is left empty. You should fill this column with the corrected '
+            'speaker id for this speaker. Leave this column empty if the speaker id does not need '
+            'to be modified. Use the special token "DELETE" to blank the speaker id for this '
+            'speaker (warning: this will affect as many medias as this speaker email is '
+            'associated with).\n'
+            '- the eights column is left empty. You should fill this column with the corrected '
+            'name for this speaker. Leave this column empty if the name does not need to be '
+            'modified. Use the special token "DELETE" to blank the name for this speaker '
+            '(warning: this will affect as many medias as this speaker email is associated with).\n'
+        ),
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument(
+        '--conf',
+        help='Path to the configuration file (e.g. myconfig.json).',
+        required=True,
+        type=str,
+    )
+    parser.add_argument(
+        '--action',
+        help='Action that the script should take. In list mode, the script will '
+             'output a CSV file of the invalid speakers in `--csv-file`. In fix mode, '
+             'the script will read the csv file from `--csv-file` and apply the fixes '
+             'to the mediaserver database.',
+        default='list',
+        choices=['list', 'fix']
+    )
+    parser.add_argument(
+        '--csv-file',
+        help='Path where the CSV file will be read from / writen to. The CSV file '
+             'uses commas (",") as a delimiter and pipes ("|") as quote characters.',
+        default=f'./invalid_speakers_{date.today().strftime("%Y-%m-%d")}.csv',
+        type=Path,
+    )
+    parser.add_argument(
+        '--apply',
+        help='Whether to apply changes or not. If not set, the script will simulate '
+             'the work and generate logs. It is a good idea to set "--log-level" to '
+             '"debug" if "--apply" is not set. This parameter only has effect in '
+             '"fix" mode (because "list" mode does not make any changes by design).',
+        action='store_true',
+    )
+    parser.add_argument(
+        '--log-level',
+        help='Log level.',
+        default='info',
+        choices=['critical', 'error', 'warn', 'info', 'debug']
+    )
+    args = parser.parse_args(sys_args)
+
+    logging.basicConfig()
+    logger.setLevel(args.log_level.upper())
+
+    msc = MediaServerClient(args.conf)
+    msc.conf['TIMEOUT'] = max(600, msc.conf['TIMEOUT'])
+
+    if args.action == 'list':
+        _list_invalid_speakers(msc, args.csv_file)
+    elif args.action == 'fix':
+        if args.apply:
+            answer = input(
+                'The script is running in normal mode. Changes from '
+                f'"{args.csv_file}" will be applied to the {msc.conf["SERVER_URL"]} database.\n'
+                'Proceed ? [y / n]'
+            )
+            if answer.lower() not in ['yes', 'y']:
+                sys.exit(0)
+        else:
+            logger.info(
+                '[Dry run] The script is running in dry-run mode. '
+                'No changes will be applied.'
+            )
+        _fix_invalid_speakers(msc, args.csv_file, args.apply)
+
+
+if __name__ == '__main__':
+    fix_invalid_speakers(sys.argv[1:])
---
+++ tests/examples/test_fix_invalid_speakers.py
@@ -0,0 +1,230 @@
+from pathlib import Path
+from unittest import mock
+
+import pytest
+
+from examples.fix_invalid_speakers import fix_invalid_speakers
+
+
+@pytest.fixture(autouse=True)
+def no_prompt():
+    with mock.patch('examples.fix_invalid_speakers.input', return_value='y') as mock_input:
+        yield mock_input
+
+
+@pytest.fixture()
+def csv_path() -> Path:
+    path = Path('./tests/examples/fix_invalid_speakers_csv_test_file.csv')
+    path.unlink(missing_ok=True)
+    yield path
+    path.unlink(missing_ok=True)
+
+
+@pytest.fixture()
+def catalog():
+    return {
+        'channels': [
+            {
+                'oid': 'channel_1',
+                'default_settings': {
+                    # Invalid email (invalid_1@example.com)
+                    'speaker_email': 'user_1@example.com|user_2@example.com|invalid_1@example.com',
+                    'speaker_id': 'user_1|user_2|invalid',  # OK
+                    'speaker': 'user one|user two|invalid',  # OK
+                }
+            },
+        ],
+        'videos': [
+            {
+                'oid': 'video_1',
+                'speaker_email': 'user_1@example.com|user_2@example.com',  # OK
+                'speaker_id': 'user_1|UsEr_TwO',  # Multiple ID (UsEr_TwO)
+                'speaker': 'user one|user two',  # OK
+            },
+            {
+                'oid': 'video_2',
+                'speaker_email': 'user_2@example.com|user_3@example.com',  # OK
+                'speaker_id': 'user_2|UsEr_thRee',  # Invalid ID (UsEr_thRee)
+                'speaker': 'user two|user 3',  # Invalid name (user 3)
+            },
+        ],
+        'lives': [
+            {
+                'oid': 'live_1',
+                # Invalid email (invalid_2@example.com)
+                'speaker_email': 'user_2@example.com|invalid_2@example.com',
+                'speaker_id': 'user_2|invalid',  # OK
+                'speaker': 'user 2|invalid',  # Multiple name (user 2)
+            },
+        ],
+    }
+
+
+@pytest.fixture()
+def users():
+    return [
+        {
+            'email': 'user_1@example.com',
+            'speaker_id': 'user_1',
+            'first_name': 'user',
+            'last_name': 'one',
+        },
+        {
+            'email': 'user_2@example.com',
+            'speaker_id': 'user_2',
+            'first_name': 'user',
+            'last_name': 'two',
+        },
+        {
+            'email': 'user_3@example.com',
+            'speaker_id': 'user_3',
+            'first_name': 'user',
+            'last_name': 'three',
+        },
+    ]
+
+
+@pytest.fixture()
+def api_client(catalog, users):
+    def mock_api_call(url, **kwargs):
+        if url == 'catalog/get-all/':
+            return catalog
+        elif url == 'users/':
+            if kwargs.get('params', {}).get('offset', 0) == 0:
+                return {'users': users}
+            else:
+                return {'users': []}
+        elif url == 'settings/defaults/metadata/edit/':
+            return {'success': True}
+        elif url == 'medias/edit/':
+            return {'success': True}
+
+    from ms_client.client import MediaServerClient
+
+    client = MediaServerClient()
+    client.api = mock.MagicMock(side_effect=mock_api_call)
+    with mock.patch('examples.fix_invalid_speakers.MediaServerClient', return_value=client):
+        yield client
+
+
+def test_list_invalid_speakers(api_client, csv_path):
+    assert not csv_path.exists()
+    fix_invalid_speakers([
+        '--conf=./conf.json',
+        '--action=list',
+        f'--csv-file={csv_path}',
+        '--log-level=info',
+    ])
+
+    # Check api calls and deleted oids
+    assert api_client.api.call_count == 2
+    assert api_client.api.call_args_list[0] == mock.call(
+        'catalog/get-all/',
+        params={'format': 'flat', 'timings': 'yes'}
+    )
+    assert api_client.api.call_args_list[1] == mock.call(
+        'users/',
+        params={'limit': 500, 'offset': 0}
+    )
+
+    # Check output
+    assert csv_path.exists()
+    csv_lines = [line for line in csv_path.read_text().split('\n') if line]
+    assert len(csv_lines) == 5
+    assert csv_lines[0] == 'email,ids,names,url,reasons,corrected_email,corrected_id,corrected_name'
+    assert set(csv_lines[1:]) == {
+        'user_2@example.com,"UsEr_TwO, user_2","user 2, user two",'
+        'https://mediaserver/permalink/live_1/,"MULTIPLE_ID, MULTIPLE_NAME",,,',
+        'user_3@example.com,UsEr_thRee,user 3,'
+        'https://mediaserver/permalink/video_2/,"INVALID_ID, INVALID_NAME",,,',
+        'invalid_1@example.com,invalid,invalid,'
+        'https://mediaserver/permalink/channel_1/,INVALID_EMAIL,,,',
+        'invalid_2@example.com,invalid,invalid,'
+        'https://mediaserver/permalink/live_1/,INVALID_EMAIL,,,',
+    }
+
+
+@pytest.mark.parametrize('apply', [True, False])
+def test_fix_invalid_speakers(api_client, csv_path, apply):
+    # Generate CSV file with corrections
+    assert not csv_path.exists()
+    csv_path.write_text('\n'.join([
+        'email,ids,names,url,reasons,corrected_email,corrected_id,corrected_name',
+        (
+            'user_2@example.com,"UsEr_TwO, user_2","user 2, user two",'
+            'https://mediaserver/permalink/live_1/,"MULTIPLE_ID, MULTIPLE_NAME",'
+            ',user_2,user two'
+        ),
+        (
+            'user_3@example.com,UsEr_thRee,user 3,'
+            'https://mediaserver/permalink/video_2/,"INVALID_ID, INVALID_NAME",'
+            ',user_3,user three'
+        ),
+        (
+            'invalid_1@example.com,invalid,invalid,'
+            'https://mediaserver/permalink/channel_1/,INVALID_EMAIL,valid_user@example.com,'
+            'valid_user,valid user'
+        ),
+        (
+            'invalid_2@example.com,invalid,invalid,'
+            'https://mediaserver/permalink/live_1/,INVALID_EMAIL,'
+            'DELETE,DELETE,DELETE'
+        ),
+    ]))
+
+    # Apply corrections to Mediaserver
+    fix_invalid_speakers([
+        '--conf=./conf.json',
+        '--action=fix',
+        f'--csv-file={csv_path}',
+        *(('--apply',) if apply else ()),
+        '--log-level=info',
+    ])
+
+    # Check api calls and deleted oids
+    assert api_client.api.call_count == (5 if apply else 1)
+    assert api_client.api.call_args_list[0] == mock.call(
+        'catalog/get-all/',
+        params={'format': 'flat', 'timings': 'yes'}
+    )
+    if apply:
+        assert api_client.api.call_args_list[1] == mock.call(
+            'settings/defaults/metadata/edit/',
+            method='post',
+            data={
+                'channel_oid': 'channel_1',
+                'speaker_email': 'user_1@example.com|user_2@example.com|valid_user@example.com',
+                'speaker_id': 'user_1|user_2|valid_user',
+                'speaker': 'user one|user two|valid user',
+            }
+        )
+        assert api_client.api.call_args_list[2] == mock.call(
+            'medias/edit/',
+            method='post',
+            data={
+                'oid': 'video_1',
+                'speaker_email': 'user_1@example.com|user_2@example.com',
+                'speaker_id': 'user_1|user_2',
+                'speaker': 'user one|user two',
+            }
+        )
+        assert api_client.api.call_args_list[3] == mock.call(
+            'medias/edit/',
+            method='post',
+            data={
+                'oid': 'video_2',
+                'speaker_email': 'user_2@example.com|user_3@example.com',
+                'speaker_id': 'user_2|user_3',
+                'speaker': 'user two|user three',
+            }
+        )
+        assert api_client.api.call_args_list[4] == mock.call(
+            'medias/edit/',
+            method='post',
+            data={
+                'oid': 'live_1',
+                'speaker_email': 'user_2@example.com',
+                'speaker_id': 'user_2',
+                'speaker': 'user two',
+            }
+        )
