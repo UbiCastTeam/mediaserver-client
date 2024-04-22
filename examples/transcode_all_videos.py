a5f5f06b20dde0186ff257f5696d8f3ee1e937e7
Florent Thiéry <florent.thiery@ubicast.eu>
2024-04-22 12:35:18 +0200

    modernize examples/transcode_all_videos.py and add purge argument

--- examples/transcode_all_videos.py
+++ examples/transcode_all_videos.py
@@ -10,42 +10,50 @@ git clone https://github.com/UbiCastTeam/mediaserver-client
 cd mediaserver-client
 python3 examples/transcode_all_videos.py
 '''
+import argparse
 import json
 import os
 import sys
 
 
-def transcode_all_videos(msc):
-    more = True
-    start = ''
-    index = 0
-    succeeded = 0
-    failed = 0
-    non_transcodable = 0
-    while more:
-        print('//// Making request on latest (start=%s)' % start)
-        response = msc.api('latest/', params=dict(start=start, content='v', count=20))
-        for item in response['items']:
-            index += 1
-            print('// Media %s: %s' % (index, item['oid']))
-            try:
-                # behavior: action to do on existing resources
-                msc.api('tasks/start/', method='post', data=dict(
+def transcode_all_videos(msc, purge):
+    non_transcodable = failed = succeeded = 0
+
+    videos = msc.api('catalog/get-all/', params={'format': 'flat'}).get(
+        'videos', list()
+    )
+    videos_count = len(videos)
+    for index, item in enumerate(videos):
+        print(f'// Media {index+1}/{videos_count}: {item["oid"]}')
+        try:
+            transcoding_params = {
+                "priority": "low",
+            }
+            if purge:
+                transcoding_params["behavior"] = "delete"
+
+            print(f"Sarting transcodings on {item['oid']}")
+            msc.api(
+                'tasks/start/',
+                method='post',
+                data=dict(
                     oid=item['oid'],
                     task='transcoding',
-                    params=json.dumps(dict(priority='low', behavior='delete'))
-                ), timeout=300)
-            except Exception as e:
-                if 'has no usable ressources' in str(e):
-                    non_transcodable += 1
-                else:
-                    print('WARNING: Failed to start transcoding task of video %s: %s' % (item['oid'], e))
-                    failed += 1
+                    params=json.dumps(transcoding_params),
+                ),
+                timeout=300,
+            )
+        except Exception as e:
+            if 'has no usable ressources' in str(e):
+                non_transcodable += 1
             else:
-                succeeded += 1
-        start = response['max_date']
-        more = response['more']
-
+                print(
+                    'WARNING: Failed to start transcoding task of video %s: %s'
+                    % (item['oid'], e)
+                )
+                failed += 1
+        else:
+            succeeded += 1
     print('%s transcoding tasks started.' % succeeded)
     print('%s transcoding tasks failed to be started.' % failed)
     print('%s media have no resouces and cannot be transcoded.' % non_transcodable)
@@ -56,8 +64,20 @@ if __name__ == '__main__':
     sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     from ms_client.client import MediaServerClient
 
-    local_conf = sys.argv[1] if len(sys.argv) > 1 else None
-    msc = MediaServerClient(local_conf)
-    msc.check_server()
+    parser = argparse.ArgumentParser()
 
-    transcode_all_videos(msc)
+    parser.add_argument(
+        '--conf', help='Path to the configuration file.', required=True, type=str
+    )
+
+    parser.add_argument(
+        '--purge',
+        action='store_true',
+        default=False,
+        help='If set, will delete all existing resources; otherwise, only missing transcodings will be generated.',
+    )
+
+    args = parser.parse_args()
+    msc = MediaServerClient(args.conf)
+    msc.check_server()
+    transcode_all_videos(msc, args.purge)
