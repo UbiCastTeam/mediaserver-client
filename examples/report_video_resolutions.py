c73aefe43d143dcb8ea09134ed16bb5501e8372f
Florent Thiéry <florent.thiery@ubicast.eu>
2024-12-13 16:35:45 +0100

    use resources-list api instead of modes

--- examples/report_video_resolutions.py
+++ examples/report_video_resolutions.py
@@ -34,47 +34,48 @@ if __name__ == '__main__':
     print('Dumping catalog')
     videos = msc.get_catalog(fmt='json')['videos']
 
-    all_modes_duration = dict()
-    all_modes_count = dict()
-    all_modes_size = dict()
+    all_resources_duration = dict()
+    all_resources_count = dict()
+    all_resources_size = dict()
 
     for index, video in enumerate(videos):
         print(f'{index + 1}/{len(videos)}', end='\r')
         oid = video['oid']
         duration = int(video['duration_s'])  # in seconds
         storage = int(video['storage_used'])  # in bytes
-        modes = msc.api('/medias/modes/', params={'oid': oid, 'all': ''})
-        if len(modes['names']) and duration:
-            largest_mode = modes['names'][0]
-            all_modes_duration.setdefault(largest_mode, 0)
-            all_modes_duration[largest_mode] += duration
-            all_modes_count.setdefault(largest_mode, 0)
-            all_modes_count[largest_mode] += 1
-            all_modes_size.setdefault(largest_mode, 0)
-            all_modes_size[largest_mode] += storage
+        resources = msc.api('/medias/resources-list/', params={'oid': oid})["resources"]
+        resources_sorted = sorted(resources, key=lambda d: d["height"], reverse=True)
+        if len(resources_sorted) > 0 and duration:
+            source_resolution = resources_sorted[0]['height']  # first one should be the largest
+            all_resources_duration.setdefault(source_resolution, 0)
+            all_resources_duration[source_resolution] += duration
+            all_resources_count.setdefault(source_resolution, 0)
+            all_resources_count[source_resolution] += 1
+            all_resources_size.setdefault(source_resolution, 0)
+            all_resources_size[source_resolution] += storage
 
     print()
     print('Source resolutions by duration :')
-    all_modes_duration = dict(
-        sorted(all_modes_duration.items(), key=lambda item: item[1], reverse=True)
+    all_resources_duration = dict(
+        sorted(all_resources_duration.items(), key=lambda item: item[1], reverse=True)
     )
-    for mode, duration in all_modes_duration.items():
-        mode_size = all_modes_size[mode]
+    for mode, duration in all_resources_duration.items():
+        mode_size = all_resources_size[mode]
         size_per_hour = int(mode_size / (duration / 3600))
         print(f'{mode}: {format_seconds(duration)}, average size: {format_bytes(size_per_hour)} per hour')
 
     print()
     print('Source resolutions by count :')
-    all_modes_count = dict(
-        sorted(all_modes_count.items(), key=lambda item: item[1], reverse=True)
+    all_resources_count = dict(
+        sorted(all_resources_count.items(), key=lambda item: item[1], reverse=True)
     )
-    for mode, count in all_modes_count.items():
+    for mode, count in all_resources_count.items():
         print(f'{mode}: {count}')
 
     print()
     print('Source resolutions by size :')
-    all_modes_size = dict(
-        sorted(all_modes_size.items(), key=lambda item: item[1], reverse=True)
+    all_resources_size = dict(
+        sorted(all_resources_size.items(), key=lambda item: item[1], reverse=True)
     )
-    for mode, size in all_modes_size.items():
+    for mode, size in all_resources_size.items():
         print(f'{mode}: {format_bytes(size)}')
